62821b33a96cfa9a176edb023a39d38584cbed31
Aquib <mohdaquib1986@gmail.com>
2023-08-22 20:38:24 +0530

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+var products=[
+    {
+        
+        name:"Airpods Wireless Bluetooth Headphones",
+        image:'/images/airpods.jpg',
+        description:'Bluetooth Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        brand:'Apple',
+        category:'Electronics',
+        price:'99.99',
+        countInStock:10,
+        rating:4.5,
+        numReviews:12,
+
+    },
+    {
+        
+        name:"boair Wireless Bluetooth Headphones",
+        image:'/images/playstation.jpg',
+        description:'Bluetooth Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        brand:'Apple',
+        category:'Electronics',
+        price:'99.99',
+        countInStock:10,
+        rating:2.5,
+        numReviews:4,
+
+    },
+    {
+       
+        name:"coair Wireless Bluetooth Headphones",
+        image:'/images/mouse.jpg',
+        description:'Bluetooth Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        brand:'Apple',
+        category:'Electronics',
+        price:'99.99',
+        countInStock:10,
+        rating:3,
+        numReviews:7,
+
+    },
+    {
+        
+        name:"doair Wireless Bluetooth Headphones",
+        image:'/images/alexa.jpg',
+        description:'Bluetooth Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        brand:'Apple',
+        category:'Electronics',
+        price:'99.99',
+        countInStock:10,
+        rating:5,
+        numReviews:8,
+
+    },
+    {
+        
+        name:"hoair Wireless Bluetooth Headphones",
+        image:'/images/camera.jpg',
+        description:'Bluetooth Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        brand:'Apple',
+        category:'Electronics',
+        price:'99.99',
+        countInStock:10,
+        rating:4,
+        numReviews:5,
+
+    },
+    {
+        
+        name:"goair Wireless Bluetooth Headphones",
+        image:'/images/phone.jpg',
+        description:'Bluetooth Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud eculpa qui officia deserunt mollit anim id est laborum.',
+        brand:'Apple',
+        category:'Electronics',
+        price:'99.99',
+        countInStock:0,
+        rating:3.5,
+        numReviews:76,
+
+    },
+]
+
+export default products
---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import {Container} from 'react-bootstrap'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import {Outlet} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+         
+        </Container>
+
+      </main>
+      <Footer/>
+      <ToastContainer/>
+    </>
+    
+    
+   
+  )
+}
+
+export default App
